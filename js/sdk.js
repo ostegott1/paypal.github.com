4e46891b67d0b16ed5bbc77a03ad248d58f75108
siddick <mebramsha@paypal.com>
2013-03-07 09:30:59 +0530

    Deploy b80ee57c3905c41ac27aa919ab686a7c0d56a32c  (b80ee57 )

---
+++ js/sdk.js
@@ -0,0 +1,19 @@
+jQuery(function($){
+
+  // Set Min-height for SDKs section
+  function set_section_height(){
+    $('.sdk-install').css({ "min-height": ($(window).height() - 150) });
+  }
+  set_section_height();
+
+  $('body').scrollspy({ target: ".sidebar", offset: 300 });
+  $('.sidebar').affix({});
+
+  $('#sdk-menus li').click(function(){
+    $('html, body').animate({
+        scrollTop: ( $($(this).children('a').attr('href')).offset().top - 150)
+    }, 400);
+    return false
+  })
+
+});
